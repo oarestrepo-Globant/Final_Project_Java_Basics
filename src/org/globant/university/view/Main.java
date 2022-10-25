95f2bcbc60839506b6d8e58edbf31e94c4f57d1f
oscar.restrepo <oscar.restrepo@globant.com>
2022-10-25 14:08:58 -0500

    feat/student

--- src/org/globant/university/view/Main.java
+++ src/org/globant/university/view/Main.java
@@ -1,7 +1,15 @@
 package org.globant.university.view;
 
+import org.globant.university.data.Student;
+
 public class Main {
     public static void main(String[] args) {
-
+        Student student1 = new Student("Oscar", "Restrepo", 33);
+        Student student2 = new Student("Andres", "Borrero", 30);
+        Student student3 = new Student("Luisa", "Farias", 25);
+        Student student4 = new Student("Daniela", "Cordoba", 22);
+        Student student5 = new Student("Henry", "Escobar", 34);
+        Student student6 = new Student("Juliana", "Rengifo", 27);
     }
+
 }
